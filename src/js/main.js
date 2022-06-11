ebd8c883573dcca5dc3a4fbda0ace672fcb39f87
Katerina <katyhvan@gmail.com>
2022-06-11 11:13:56 +0600

    add changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,28 @@
 import $ from 'jquery';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use({ Navigation, Pagination });
+
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
+
+
+let hamburger = $('.hamburger');
+let nav = $('.header__nav');
+
+hamburger.on('click', function () {
+  hamburger.toggleClass('is-active')
+  nav.toggleClass('is-active')
+})
 
 
 $(function (){
